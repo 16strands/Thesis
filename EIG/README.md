e5d4721c19a3d615e875c1159417a5a455b62c74
Sarah Strand <16strands@gmail.com>
2019-12-11 22:43:49 -0800

    Cleaned up stupid errors, moved general questions to README

--- EIG/README.md
+++ EIG/README.md
@@ -9,3 +9,34 @@
 * How do I step through time?
     - priority queue won't wait between items, so there is no passing of time really
         - queue things to happen in between events to account for the passing of time? (like if the latency is 200, then either 200 events, or one type of event that "sleeps" for 200 ms?)
+        
+        
+# Who should keep track of the event buffer? (aka is it global or separate for each node)
+# Is the latency constant? 
+# SimPy ?
+# How to actually measure the stuff?
+
+# store in list until all messageds in round received
+# then enter in EIG tree?
+#  has a timeout counter and then assumed "bot"
+    # where to enter timeout thing?
+
+# eig as subroutine might be weird but gotta do it 
+# worst case running time when everyone is honest?
+# what things can be tweaked for speed boost?
+# how to make things asynchronous 
+# failures vs byzantine failures
+# how can byz failures slow this down? like logical adversaries
+# how can we achieve the worst case bound         
+
+
+# NETWORK_LATENCY = 200 # Or does each node have its own latency? 
+# enqueue timeout after each send? like "if i dont receive anythign within 200ms then stoP?"
+
+
+# Maybe not a proirity queue!!! Since things can actually happen at the same time!! 
+# maybe just a list of lists ordered by the time specified ?? 
+# OR MAYBE PRIORITY QUEUE CAN TRIGGER EVENTS AT THE SAME TIME IF THEY HAVE THE SAME PRIORITY
+# no, apparently they're served in the order they appear, but maube that's good enough?
+# but i can't code in the queue being timed bc everythign is gonna happen so fast 
+
--- EIG/node.py
+++ EIG/node.py
@@ -1,42 +1,24 @@
-## QUESTIONS ##
-# Who should keep track of the event buffer? (aka is it global or separate for each node)
-# Is the latency constant? 
-# SimPy
-# How to actually measure the stuff?
-
 class Node():
     def __init__(self, byz, name, val):
         self.byz = byz
         self.name = name # is this necessary?
         self.initialValue = val
+        self.round = 0
+        self.tree = {("root", self.initialValue)} # Figure out how to structure this better
 
-    self.round = 0
-    self.tree = {(root, initialValue)}
-
-    def decide():
+    def decide(self):
         pass
 
-    def broadcast():  # return the value to be brodcasted
+    def broadcast(self):  # return the value to be brodcasted
         pass
         #call some global function with sender, receiver and time as input, and latency
         # system call to send messages 
 
-    def receive() : # event triggers at time specified at broadcast time 
-    # store in list until all messageds in round received
-    # then enter in EIG tree?
-    #  has a timeout counter and then assumed "bot"
-        # where to enter timeout thing?
-
-    # eig as subroutine might be weird but gotta do it 
-    # worst case running time when everyone is honest?
-    # what things can be tweaked for speed boost?
-    # how to make things asynchronous 
-    # failures vs byzantine failures
-    # how can byz failures slow this down? like logical adversaries
-    # how can we achieve the worst case bound 
+    def receive(self): # event triggers at time specified at broadcast time
+        pass
         
 
-class EIGNode():
+class EIGNode():  # what does an EIG tree even look like bruh...
     def __init__(self, level, val, children):
         self.level = level
         self.val = val
